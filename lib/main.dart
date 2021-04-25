ecbbace2aa54acfa8134c1e5862486feb5ddd2cf
Leonardo <leoenav@gmail.com>
2021-04-25 11:03:08 -0300

    Tres formas diferentes de passar uma funcao apra o onpressed do buttom

--- lib/main.dart
+++ lib/main.dart
@@ -3,14 +3,45 @@ import 'package:flutter/material.dart';
 main() => runApp(PerguntaApp());
 
 class PerguntaApp extends StatelessWidget {
+  void responder() {
+    print('Pergunta Respondida!');
+  }
+
+  void Function() funcaoQueRetornaUmaOutraFuncao() {
+    return () {
+      print('Pergunta respondida #01!');
+    };
+  }
+
   @override
   Widget build(BuildContext context) {
+    final List<String> perguntas = [
+      'Qual é a sua cor favorita?',
+      'Qual é o seu animal favorito?',
+    ];
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text("Perguntas"),
         ),
-        body: Text("Olá Flutter"),
+        body: Column(
+          children: <Widget>[
+            Text(perguntas[0]),
+            ElevatedButton(
+              child: Text('Resposta 1'),
+              onPressed: funcaoQueRetornaUmaOutraFuncao(),
+            ),
+            ElevatedButton(
+              child: Text('Resposta 2'),
+              onPressed: () => print('Resposta 2 selecionada!'),
+            ),
+            ElevatedButton(
+              child: Text('Resposta 3'),
+              onPressed: responder,
+            ),
+          ],
+        ),
       ),
     );
   }
